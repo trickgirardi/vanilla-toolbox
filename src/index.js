0c6c6b5890f8a4b6ef4f1bf8914f4f01c01ae14f
isTrick <patrickgirardidossantos@gmail.com>
2024-09-17 09:35:02 -0300

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import taskController from './controller/task.js'
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+
+app.use('/task', taskController)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
